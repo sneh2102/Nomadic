b0eaa15d3a45275427a9a8323e874cc37634a3b3
Meer Kamleshkumar Patel <mr418607@dal.ca>
2024-06-20 11:14:54 -0300

    initial template

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { TextField } from '@mui/material';
+
+function App() {
+  return (
+    <div className="">
+    </div>
+  );
+}
+
+export default App;
