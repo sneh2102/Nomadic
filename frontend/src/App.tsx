25ce19977029591254c5758d4fa52c8285151a02
Meer Kamleshkumar Patel <mr418607@dal.ca>
2024-06-20 12:23:50 -0300

    starter template with routes and folder structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { TextField } from '@mui/material';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import FAQPage from './pages/FAQPage';
+import HomePage from './pages/HomePage';
+import ContactUsPage from './pages/ContactUsPage';
 function App() {
   return (
-    <div className="">
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/faq' element={<FAQPage />} />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/contactus' element={<ContactUsPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/ContactUsPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ContactUs from '../components/ContactUs'
+
+const ContactUsPage = () => {
+    return (
+        <div><ContactUs /></div>
+    )
+}
+
+export default ContactUsPage
