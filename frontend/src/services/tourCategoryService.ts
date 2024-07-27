f6c521eda9308b8a61fdb76da40cc132b3bb16ad
Smit Patel <smit.patel@dal.ca>
2024-07-27 11:30:04 -0300

    add secrets

--- frontend/src/services/tourCategoryService.ts
+++ frontend/src/services/tourCategoryService.ts
@@ -1,6 +1,6 @@
 export const getAllTourCategories = async () => {
     try {
-        const response = await fetch('http://localhost:8000/api/v1/tour-categories');
+        const response = await fetch(`${import.meta.env.VITE_BASE_API_URL}/api/v1/tour-categories`);
         if (!response.ok) {
         throw new Error('Failed to fetch');
         }
